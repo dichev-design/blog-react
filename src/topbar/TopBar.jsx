d6346c6e46ce3abdd030cf6b2889cafa370b3920
dichev-design <dichev.official@gmail.com>
2024-12-07 17:03:27 +0200

    React-Blog

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,13 @@
+import "./topbar.css"    
+
+export default function TopBar() {
+  return (
+    <div className="top">
+        <div className="topleft">
+        <i class="fa-brands fa-square-facebook"></i>
+        </div>
+        <div className="topcenter">c</div>
+        <div className="topright">r</div>
+    </div>
+  )
+}
